1dfecb32f00ce8ce113ec10b85446dd4ceb26d5a
Christian <christian_d_ol9000@hotmail.com>
2020-11-11 23:51:46 -0300

    começando' git commit -m começando git add .

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace projeto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite o primeiro número: ");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("digite o segundo número: ");
+            int n2 = int.Parse(Console.ReadLine());
+            int soma = n1 + n2;
+            Console.WriteLine("resultado= " + soma);
+        }
+    }
+}
